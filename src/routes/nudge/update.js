205d2e9e54f04c00f8c3b8c43a37c927aa328cca
Gabriel <gabriel.debeaupuis@gmail.com>
2019-12-05 17:53:27 -0800

    lint

--- src/routes/nudge/update.js
+++ src/routes/nudge/update.js
@@ -4,11 +4,7 @@ import { Link, useHistory } from 'react-router-dom';
 
 import FormNudge from '../../components/form-nudge';
 import { PATH as DashboardPath } from '../dashboard';
-import { useMutation } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
 import { useNudgeApi } from '../../hooks/use-nudge-api';
-
-import UPDATE_NUDGE from '../../graphql/mutations/update-nudge';
 
 export const PATH = '/nudge/update/:id';
 export const pathFor = (id) => PATH.replace(':id', id);
@@ -21,17 +17,11 @@ function NudgeUpdate(props) {
   const { getNudge, updateNudge } = useNudgeApi();
   const history = useHistory();
 
-  const { loading, error, data } = getNudge({ id: params.id });
+  const { loading, error, data } = getNudge({ variables: { id: params.id } });
 
-  const [mut] = useMutation(UPDATE_NUDGE, {
-
-  });
 
   // handlers
-  const handleSubmit = async (variables) => {
-    await mut({ variables });
-    history.push(DashboardPath);
-  };
+  const handleSubmit = (variables) => updateNudge(variables).then(() => history.push(DashboardPath));
 
   if (loading) return 'Loading...';
   if (error) return 'Error!';
