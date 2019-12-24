9c25d6051990e7a8e01c864c04aece00a8285777
Gabriel <gabriel.debeaupuis@gmail.com>
2019-12-24 14:42:50 +0100

    fix eslint

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+export { PATH as LoginPath } from './login';
+export { PATH as SignupPath } from './signup';
+export { PATH as GoalNewPath } from './goal/new';
+export { PATH as GoalUpdatePath } from './goal/update';
+export { PATH as DashboardPath } from './dashboard';
+
+export default {};
